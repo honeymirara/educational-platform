35eff1736c72b76252cc33281bd00e7d26d37b98
honeymirara <honeymirara@gmail.com>
2023-08-06 14:17:46 -1000

    edits-regPage

--- client/src/page/RegPage/RegPage.js
+++ client/src/page/RegPage/RegPage.js
@@ -1,12 +1,33 @@
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
+import style from './style.module.css'
 
 
 export default function RegPage() {
     return (
         <div>
             <Header />
-            <div className={style.wrapper}></div>
+            <div className={style.wrapper}>
+                <div className={style.info}></div>
+                <div className={style.signUp}>
+                    <h1>Sign Up</h1>
+                    <div>
+                        <input type="text" />
+                    </div>
+                    <div>
+                        <input type="text" />
+                    </div>
+                    <div>
+                        <input type="text" />
+                    </div>
+                    <div >
+                        <input type="text" />
+                    </div>
+                    <div className={style.btn}>Sign Up</div>
+                </div>
+
+                <div className={style.signUpImg}></div>
+            </div>
             <Footer />
         </div >
 
