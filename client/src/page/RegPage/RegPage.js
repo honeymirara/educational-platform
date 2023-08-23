21ab0f5f4f2161974fa828b24b660329acb128d6
honeymirara <honeymirara@gmail.com>
2023-08-23 11:11:26 -0900

    useState

--- client/src/page/RegPage/RegPage.js
+++ client/src/page/RegPage/RegPage.js
@@ -2,10 +2,15 @@ import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
 import style from './style.module.css'
 import Input from '../../components/Input/Input'
+import { useState } from 'react'
 
 
 export default function RegPage() {
-const arr = ['name', 'last name', 'email', 'pwd'];
+
+    const [value, setValue] = useSate({ name: '', surname: '', email: '', pwd: '' });
+    const arr = ['name', 'surname', 'email', 'pwd'];
+
+
     return (
         <div>
             <Header />
@@ -13,7 +18,7 @@ const arr = ['name', 'last name', 'email', 'pwd'];
                 <div className={style.info}></div>
                 <div className={style.signUp}>
                     <h1>Sign on</h1>
-                    <Input arr = {arr}/>
+                    <Input arr={arr} />
                     <div className={style.btn}>Sign Up</div>
                 </div>
 
