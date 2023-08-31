7a77865d2b0be9d6a87aabc5df1964c80219e2db
honeymirara <honeymirara@gmail.com>
2023-08-31 13:03:52 +0300

    page-student-style

--- client/src/page/StudentPage/StudentPage.js
+++ client/src/page/StudentPage/StudentPage.js
@@ -1,11 +1,42 @@
 import Header from "../../components/Header/Header"
 import Footer from '../../components/Footer/Footer'
+import style from './style.module.css';
 
 export default function StudentPage() {
+    const arr = [{
+        name: 'Javascript',
+        text: 'JavaScript is a practical course where students learn the basics of JavaScript. It covers variables, operators, conditionals, loops, functions, and data manipulation.'
+    },
+    {
+        name: 'Typescript',
+        text: "TypeScript is a course that provides an introduction to TypeScript. Students will learn about TypeScript's key features, such as type annotations, interfaces, classes, and modules"
+    },
+    {
+        name: 'Python',
+        text: 'Students will learn about variables, data types, conditionals, loops, functions, and file handling. Through hands-on exercises and projects, students will gain proficiency in writing Python code and solving real-world problems.'
+    }];
+
     return (
         <>
             <Header />
-            <div>
+            <div className={style.wrapper}>
+                <div className={style.container}>
+                    <div className={style.image}></div>
+                </div>
+                <div className={style.list}>
+                    {
+                        arr.map((el) => (
+                            <div className={style.courseContainer}>
+                            <div className={style.imageCourse}>mo-img</div>
+                            <div className={style.informationCourse}>
+                                <h2>{el.name}</h2>
+                                <p>{el.text}</p>
+                            </div>
+                        </div>
+                        )
+                        )
+                    }
+                </div>
                 <h1>Страница Студента</h1>
             </div>
             <Footer />
