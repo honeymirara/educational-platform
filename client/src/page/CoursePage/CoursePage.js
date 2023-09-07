3d312fdd9b4b490d2139349321e50f1cafc2244a
honeymirara <honeymirara@gmail.com>
2023-09-07 11:47:31 +0300

    id

--- client/src/page/CoursePage/CoursePage.js
+++ client/src/page/CoursePage/CoursePage.js
@@ -1,8 +1,13 @@
-export default function CoursePage() {
+import { useParams } from 'react-router-dom';
 
+
+
+export default function CoursePage() {
+    const { id } = useParams()
     return (
         <>
             <h1>Good morning!!!</h1>
+            <p>{id}</p>
         </>
     )
 }
